23308cd8519b8ef577f7352b4cdd4a91552a64b7
sarankumar003 <sarankumar1326@gmail.com>
2025-10-17 12:08:20 +0530

    start configs modified

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
   },
+  experimental: {
+    esmExternals: false,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
